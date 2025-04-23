6d905819b3f946153e38976c630fd92f246b627b
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-23 17:59:19 +0800

    Update invoice_processing.py

--- utils/invoice_processing.py
+++ utils/invoice_processing.py
@@ -169,7 +169,7 @@ def format_final_invoice(user_data: Dict) -> str:
         message += line_str
     
     message += "-" * 70 + "\n"
-    message += f"{'ИТОГО:':<45} {total:<10.2f}\n"
+    message += "ИТОГО:                                            " + f"{total:<10.2f}\n"
     message += "```\n\n"
     
     # Добавляем информацию о действиях
