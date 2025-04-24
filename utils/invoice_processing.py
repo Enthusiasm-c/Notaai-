c325831dbb7713889639b9dc49a6ae3598aabcaf
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-24 21:52:51 +0800

    Update invoice_processing.py

--- utils/invoice_processing.py
+++ utils/invoice_processing.py
@@ -5,7 +5,7 @@
 import dataclasses
 import datetime
 import logging
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple
 
 from utils.learning import load_unit_conversions
 
