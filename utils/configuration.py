4d07fc852c77843f7c9f1413d7f9d3c4788608a9
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-24 11:40:17 +0800

    Update configuration.py

--- utils/configuration.py
+++ utils/configuration.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import Optional
 
 from pydantic_settings import BaseSettings
-from pydantic import Field, ValidationError
+from pydantic import Field
 
 
 class Config(BaseSettings):
